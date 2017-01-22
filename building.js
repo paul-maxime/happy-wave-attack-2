e35b1667442058154fbb4f41953f24751de571e9
Paul-Maxime <github@paulmaxime.fr>
2017-01-22 12:32:15 +0100

    Add the perfect destroyable building

--- building.js
+++ building.js
@@ -4,7 +4,7 @@ class Building
 {
 	constructor() {
 		this.sprite = game.add.sprite(0, 0, this.getTexture(), null, waveAttack.humansGroup);
-		this.sprite.anchor.setTo(0, 1);
+		this.sprite.anchor.setTo(0.5, 1);
 		this.sprite.x = game.world.width + this.sprite.height;
 		this.sprite.y = game.world.height;
 		let scale = game.rnd.integerInRange(30, 50) / 10;
@@ -24,12 +24,20 @@ class Building
 	update(deltaTime) {
 		if (!this.isBlocking) {
 			this.sprite.x += deltaTime * this.speedX;
-			if (this.sprite.x < game.wave.width / 2 && game.waveUp) {
+			if (this.sprite.x < game.wave.width) {
 				this.isBlocking = true;
 			}
 		} else {
 			if (game.isSpaceDown() && !this.isAttacking) {
 				this.isAttacking = true;
+				game.playExplosion();
+				game.humansKilled += 1;
+				game.updateWaveColor(1);
+				if (this.life % 3 == 0) {
+					game.playScream();
+				}
+				this.sprite.rotation += 0.05;
+				this.sprite.position.y += 9;
 				this.life -= 1;
 			} else if (!game.isSpaceDown() && this.isAttacking) {
 				this.isAttacking = false;
