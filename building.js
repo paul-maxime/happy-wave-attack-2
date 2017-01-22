95e738263c6370f1e604a53429db097c09119f32
Paul-Maxime <github@paulmaxime.fr>
2017-01-22 13:17:49 +0100

    Building is now spawning correctly, lol

--- building.js
+++ building.js
@@ -5,8 +5,6 @@ class Building
 	constructor() {
 		this.sprite = game.add.sprite(0, 0, this.getTexture(), null, waveAttack.humansGroup);
 		this.sprite.anchor.setTo(0.5, 1);
-		this.sprite.x = game.world.width + this.sprite.width;
-		this.sprite.y = game.world.height;
 		this.humanSprite = game.add.sprite(0, 0, this.getHumanText(), null, waveAttack.humansGroup);
 		this.humanSprite.anchor.setTo(0.5, 1);
 		this.humanSprite.x = game.world.width + this.sprite.height;
@@ -17,6 +15,9 @@ class Building
 		let scale = game.rnd.integerInRange(40, 60) / 10;
 		this.sprite.scale.setTo(scale, scale);
 		this.humanSprite.scale.setTo(scale, scale);
+
+		this.sprite.x = game.world.width + this.sprite.width;
+		this.sprite.y = game.world.height;
 
 		this.sprite.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
 		this.humanSprite.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
