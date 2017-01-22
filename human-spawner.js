bcbb59f9fe6e930fcea851dc9a3bdcb80ad5ff37
Paul-Maxime <github@paulmaxime.fr>
2017-01-22 11:16:47 +0100

    Improve the human and fan spawning

--- human-spawner.js
+++ human-spawner.js
@@ -2,16 +2,31 @@
 
 class HumanSpawner {
 	constructor () {
-		this.spawnNextHuman();
+		this.totalTime = 0;
+		this.nextHuman = 0;
+		this.nextEnemy = 0;
 	}
 	spawnNextHuman () {
-		this.nextHuman = game.rnd.integerInRange(300, 500);
-		waveAttack.humans.push(new Human());
+		let frequency = 10000 / ((this.totalTime / 20) + 1);
+		this.nextHuman = game.rnd.integerInRange(frequency / 2, frequency);
+		waveAttack.humans.push(new Human(false));
+	}
+	spawnNextEnemy () {
+		let frequency = 10000 / ((game.humansKilled + 1) / 2);
+		this.nextEnemy = game.rnd.integerInRange(frequency / 2, frequency);
+		waveAttack.humans.push(new Human(true));
 	}
 	update (deltaTime) {
+		this.totalTime += deltaTime;
 		this.nextHuman -= deltaTime * 1000;
 		if (this.nextHuman < 0) {
 			this.spawnNextHuman();
+		}
+		if (game.humansKilled > 0) {
+			this.nextEnemy -= deltaTime * 1000;
+			if (this.nextEnemy < 0) {
+				this.spawnNextEnemy();
+			}
 		}
 	}
 }
